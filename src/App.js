f8305a5f1a8f36ca6fd668f457b2c9789cbcdb72
Patience Fuglo <pfuglo@fordham.edu>
2020-12-05 12:02:50 -0500

    Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import{ BrowserRouter, Switch, Route} from "react-router-dom";
+import Home from "./Components/Home";
+import Quiz from "./Components/Quiz";
+import Score from "./Components/Score";
 
 function App() {
   return (
+    <BrowserRouter>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+         <Route path="/Home">
+           <Home />
+         </Route>
+         <Route path="/Quiz">
+           <Quiz>
+           </Quiz>
+         </Route>
+         <Route path="/Score">
+           <Score>
+           </Score>
+         </Route>
+      </Switch>
     </div>
+    </BrowserRouter>
+
   );
 }
 
